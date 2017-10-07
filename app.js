aa1e58d7abbb9e4e8e1171ec35c912c70d7e5798
Saugat Tripathi <saugat.tripathi@bison.howard.edu>
2017-10-07 14:01:14 -0400

    First base files have been added

---
+++ app.js
@@ -0,0 +1,12 @@
+
+  //
+  var config = {
+    apiKey: "AIzaSyDKgu6IMSSPjMDymFh_ivaS-rDrWljjluQ",
+    authDomain: "hu-safe.firebaseapp.com",
+    databaseURL: "https://hu-safe.firebaseio.com",
+    projectId: "hu-safe",
+    storageBucket: "hu-safe.appspot.com",
+    messagingSenderId: "633905449890"
+  };
+  firebase.initializeApp(config);
+</script>
